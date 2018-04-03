7e8fdd8b8f5891fd78cdddb1ebaed27abc8b202b
enginebeat <luis.moreira@lmtronics.co.uk>
2018-04-03 21:26:28 +0000

    some changes to make sure that the server starts before anything else is allowed to go. Noticed some issues with the output7 & output11 setup that I don't like...

--- iotest.js
+++ iotest.js
@@ -1,30 +1,23 @@
 console.log('app started');
 
-
-//const raspi = require('raspi');
-//const gpio = require('raspi-gpio');
-
-//raspi.init(() => {
-  //const input = new gpio.DigitalInput('P1-3');
-  //const output = new gpio.DigitalOutput('P1-7');
-  //output.write(1);
-//});
-
 /***************** ServerController **************/
 var ServerController = (()=>{
   
     const raspi = require('raspi');
     const gpio = require('raspi-gpio');
-    var output;
-
-    var express = require('express');
+    
+    const express = require('express');
     var app = express();
+    
     app.use(express.static('./public'));
 
-    var socket = require("socket.io");
+    const socket = require("socket.io");
 
-    var server = app.listen(9000,'0.0.0.0', ()=>{
-        console.log('server started on port 9000')
+    const server = app.listen(9000,'0.0.0.0', ()=>{
+        console.log('server started on port 9000');
+        //to make sure the the server is listening 
+        //before socket is created 
+        initSocket();
 
     });
 
@@ -33,11 +26,14 @@ var ServerController = (()=>{
     //var networkInterfaces = os.networkInterfaces( );
 
     //console.log( networkInterfaces );
+    
+    function initSocket(){
 
-
-    raspi.init(() => {
-        output11 = new gpio.DigitalOutput('P1-11');
-        output7 = new gpio.DigitalOutput('P1-7');
+        raspi.init(() => {
+            
+            output11 = new gpio.DigitalOutput('P1-11');
+            output7 = new gpio.DigitalOutput('P1-7');
+        });
 
         //Socket setup
         var io = socket(server);
@@ -54,9 +50,7 @@ var ServerController = (()=>{
                 }else if(data.pinNum === '11'){
                     console.log('in 11');
                     output11.write(1);
-                };
-                
-
+                };  
             });
             socket.on('offButton', (data)=>{
                 var pinNumber;
@@ -67,9 +61,10 @@ var ServerController = (()=>{
                     output11.write(0);
                 };
                 
-            })
+            });
         });
-    });
+    
+    };
 
     
 
